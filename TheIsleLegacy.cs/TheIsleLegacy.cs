32cd24e9a3b544a877e99e2d87c01e2db0f877d4
Jesper Lund <jesperlund913@hotmail.com>
2023-01-30 10:46:36 +0000

    Added option for AdminLists and better fix for defaulting gamemodes (if not specified etc)

--- TheIsleLegacy.cs/TheIsleLegacy.cs
+++ TheIsleLegacy.cs/TheIsleLegacy.cs
@@ -8,6 +8,7 @@ using WindowsGSM.GameServer.Engine;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 
 
 namespace WindowsGSM.Plugins
@@ -86,13 +87,102 @@ namespace WindowsGSM.Plugins
             string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
 
             // Prepare start parameter
+
+            /*Here we wanna update our Game.ini to have the most "Recent Server Name" from the GSM Program - instead of making it just keep the original name from CFG on creation.
+            First we of course check the file exists
+            - If not existing, we will download a fresh one.
+            - If existing, we will update the server name from WGSM into Game.ini
+            
+             */
+
+            string configPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, @"TheIsle\Saved\Config\WindowsServer\Game.ini");
+            Directory.CreateDirectory(Path.GetDirectoryName(configPath));
+
+            if (await adaptGameIniOnLaunch(configPath, configPath))
+            {
+                //Server Name Values
+                string section = "/Script/TheIsle.IGameSession";
+                string newServerNameValue = _serverData.ServerName;
+                string serverNameKey = "ServerName";
+
+                string[] lines = File.ReadAllLines(configPath);
+                bool foundSection = false;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Trim().Equals("[" + section + "]"))
+                    {
+                        foundSection = true;
+                        continue;
+                    }
+                    if (foundSection && lines[i].Trim().StartsWith(serverNameKey))
+                    {
+                        string[] parts = lines[i].Split('=');
+                        if (parts.Length >= 2 && !parts[1].Equals(newServerNameValue))
+                        {
+                            lines[i] = serverNameKey + "=" + newServerNameValue;
+                            File.WriteAllLines(configPath, lines);
+                            System.Diagnostics.Debug.WriteLine("Value updated in file: " + configPath);
+                            break;
+                        }
+                        else
+                        {
+                            System.Diagnostics.Debug.WriteLine("Value already exists in file: " + configPath);
+                            break;
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+
+            /*
+               Update the Game.ini ServerAdmins= with pre-set adminfiles containing Steam IDS if existing.
+               
+               Admin List Mode - OBS: NOT REQUIRED
+               - Our goal here is to make adding admins on multiple servers easier than having to manually adjust each server every time they change admins
+
+               Lets load an an admin list from a text file, but only if the adminList=XXX exists in the Param.
+               - example: 
+               game=Survival;adminList=https://raw.githubusercontent.com/menix1337/WindowsGSM.configs/main/Asura/TheIsleLegacy/Adminlist.txt
+
+               Some servers have different admins depending if the server is a Deathmatch type server or not - to help this we can add in a second admin list (adminListTwo) & combine the adminList & adminListTwo
+               - example: 
+               game=Survival;adminList=https://raw.githubusercontent.com/menix1337/WindowsGSM.configs/main/Asura/TheIsleLegacy/Adminlist.txt;adminListTwo=https://raw.githubusercontent.com/menix1337/WindowsGSM.configs/main/Asura/TheIsleLegacy/AdminlistDM.txt
+
+               Then the server will merge and update the lists into the game.ini with the IDs of each line in the text files
+
+               adminList.txt file example (Have each id on each line):
+               76561197960419839
+               76561197960419840
+               76561197960419841
+               76561197960419842
+
+               then adminListTwo.txt file example:
+               76561197960419843
+
+               You can also add more lists like adminListThree.txt, adminListFour.txt
+               - just make sure each list starts with adminList
+
+               Will combine into a total of this when put into the game ini:
+               ServerAdmins=76561197960419839
+               ServerAdmins=76561197960419840
+               ServerAdmins=76561197960419841
+               ServerAdmins=76561197960419842
+               ServerAdmins=76561197960419843
+
+               OBS: If admin list is specified, for each time you restart the server it will clear out all admins and re-apply accordingly from the list; if the source .txt files can be found - otherwise it will keep the original game.ini without refreshing the admins (In case the source is down so you suddenly dont have admin)
+            */
+
+            await UpdateAdminList(_serverData.ServerParam, configPath);
+
+
             /*
             if server default map contains either Isle V3 or Thenyaw or DV_TestLevel then add the string
             /Game/TheIsle/Maps/Landscape3/Isle_V3 for Isle V3
             /Game/TheIsle/Maps/Thenyaw_Island/Thenyaw_Island for Thenyaw
             /Game/TheIsle/Maps/Developer/DV_TestLevel for Dev Map
             */
-
             List<string> IsleV3Variations = new List<string>() { "Isle V3", "isle v3", "v3", "islev3" };
             List<string> ThenyawVariations = new List<string>() { "Thenyaw", "thenyaw", "ThenyawIsland", "Thenyaw Island" };
             List<string> TestlevelVariations = new List<string>() { "testlevel", "DV_TestLevel", "dm", "Test Level", "Dev Map", "Dev level" };
@@ -115,11 +205,14 @@ namespace WindowsGSM.Plugins
                 param = string.Empty;
             }
 
+
+            string gameMode = await GetGameMode(_serverData.ServerParam);
+
             param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?MultiHome={_serverData.ServerIP}";
             param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?Port={_serverData.ServerPort}";
             param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?QueryPort={_serverData.ServerQueryPort}";
             param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?MaxPlayers={_serverData.ServerMaxPlayer}";
-            param += $"?{_serverData.ServerParam} -nosteamclient -game -server -log";
+            param += $"?{gameMode} -nosteamclient -game -server -log";
 
             // Prepare Process
             var p = new Process
@@ -219,5 +312,107 @@ namespace WindowsGSM.Plugins
 
             return File.Exists(filePath);
         }
+
+        public static async Task<bool> adaptGameIniOnLaunch(string fileSource, string filePath)
+        {
+
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            //if the file DOESN'T exist - lets re-create it.
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    using (WebClient webClient = new WebClient())
+                    {
+                        await webClient.DownloadFileTaskAsync($"https://raw.githubusercontent.com/menix1337/WindowsGSM.configs/main/TheIsleLegacy/Game.ini", filePath);
+                    }
+                }
+                catch (Exception e) {
+                    System.Diagnostics.Debug.WriteLine($"Github.DownloadGameServerConfig {e}"); 
+                }
+            }
+            return File.Exists(filePath);
+        }
+
+        public static async Task<string> GetGameMode(string serverData)
+        {
+            string defaultGameMode = "game=Survival";
+            string[] parts = serverData.Split(';');
+            foreach (var part in parts)
+            {
+                if (part.StartsWith("game=", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (part.Equals("game=Survival", StringComparison.OrdinalIgnoreCase) ||
+                        part.Equals("game=Sandbox", StringComparison.OrdinalIgnoreCase))
+                    {
+                        defaultGameMode = part;
+                        break;
+                    }
+                }
+            }
+
+            return defaultGameMode;
+        }
+
+        public static async Task UpdateAdminList(string _serverData, string gameIniPath)
+        {
+            string[] splitServerData = _serverData.Split(';');
+            Dictionary<string, string> adminListFiles = new Dictionary<string, string>();
+            foreach (string s in splitServerData)
+            {
+                if (s.StartsWith("adminList", StringComparison.OrdinalIgnoreCase))
+                {
+                    string[] splitAdminList = s.Split('=');
+                    adminListFiles.Add(splitAdminList[0], splitAdminList[1]);
+                }
+            }
+            List<string> combinedAdminList = new List<string>();
+            foreach (KeyValuePair<string, string> kvp in adminListFiles)
+            {
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        string txtFile = await client.DownloadStringTaskAsync(kvp.Value);
+                        string[] lines = txtFile.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                        foreach (string line in lines)
+                        {
+                            combinedAdminList.Add(line.Trim());
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            if (combinedAdminList.Count > 0)
+            {
+                var lines = File.ReadAllLines(gameIniPath).ToList();
+                int startIndex = lines.FindIndex(x => x.StartsWith("[/Script/TheIsle.IGameSession]"));
+                int endIndex = lines.FindIndex(startIndex, x => x.StartsWith("[/script/theisle.igamemode]"));
+                int currentIndex = startIndex + 1;
+                while (currentIndex < endIndex)
+                {
+                    if (lines[currentIndex].StartsWith("ServerAdmins="))
+                    {
+                        lines.RemoveAt(currentIndex);
+                        endIndex--;
+                    }
+                    else
+                    {
+                        currentIndex++;
+                    }
+                }
+                int insertIndex = endIndex;
+                foreach (string adminId in combinedAdminList)
+                {
+                    lines.Insert(insertIndex, $"ServerAdmins={adminId}");
+                    insertIndex++;
+                }
+                File.WriteAllLines(gameIniPath, lines);
+            }
+        }
+
     }
 }
