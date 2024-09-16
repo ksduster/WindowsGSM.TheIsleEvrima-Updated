90df2e3cd952e356ace2c143ccff96a1deeb8c52
Duster <ksduster@yahoo.com>
2024-09-15 23:50:40 -0500

    Update TheIsle.cs

--- TheIsle.cs/TheIsle.cs
+++ TheIsle.cs/TheIsle.cs
@@ -298,10 +298,10 @@ namespace WindowsGSM.Plugins
 
             //since the ServerStartParam can have multiple things here (such as adminLists) we divide it up using GetGameMode - to make sure we only put the gamemode into the actual Start Param of our game server. GetGameMode() splits out the relevant information to specify gamemode
 
-            //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?MultiHome={_serverData.ServerIP}";
+            //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?MultiHome={_serverData.ServerIP}";  // Multihome is depreciated now for Evrima.
             param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?Port={_serverData.ServerPort}";
-            //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?QueryPort={_serverData.ServerQueryPort}";
-            param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?MaxPlayers={_serverData.ServerMaxPlayer}";
+            //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?QueryPort={_serverData.ServerQueryPort}";  // Query port is depreciated in Evrima
+            //param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $"?MaxPlayers={_serverData.ServerMaxPlayer}"; // Player limit is set in Game.ini now
             param += $"? -nosteamclient -game -server -log";
 
             // Prepare Process
